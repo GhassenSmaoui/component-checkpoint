a31f78bbbd9eac9bedac933ee0c0ad1d307640b8
GhassenSmaoui <smaouighassen@yahoo.fr>
2020-12-16 21:35:10 +0100

    first commit

---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Address() {
+  return (
+    <div>
+      <h1 className=" red"> Manzel Chaker street Sfax Tunisia</h1>
+    </div>
+  );
+}
+
+export default Address;
---
+++ src/Component/Profile/ProfilPhoto.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProfilPhoto = () => {
+  return (
+    <div>
+      <img style={{width:'300 px', height:'300px'}} src="./myphoto.png" alt="myphoto" />
+    </div>
+  );
+};
+
+export default ProfilPhoto;
